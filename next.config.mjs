cac2e74832a8b95e8959ccb166c9e1b862b0fed7
Energy burner of motivation <prakashbabuparamhans729srmt@gmail.com>
2025-11-26 07:31:11 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,13 @@
+import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const withPWA = require('next-pwa')({
-    dest: 'public'
-})
+const pwaConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
 
-const nextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-};
-
-export default withPWA(nextConfig);
+export default pwaConfig(nextConfig);
